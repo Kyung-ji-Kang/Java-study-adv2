8984bf1e0a5ab53a66585790b950de889d62c979
강경지 <kangkj001@gmail.com>
2025-04-27 15:17:44 +0900

    20250427

---
+++ src/network/connect/SoTimeoutClient.java
@@ -0,0 +1,27 @@
+package network.connect;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class SoTimeoutClient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("localhost",12345);
+        InputStream input = socket.getInputStream();
+
+        try{
+            socket.setSoTimeout(3000); // 타임아웃 시간 설정
+            int read = input.read();
+            System.out.println("read = "+ read);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        socket.close();
+
+    }
+}
