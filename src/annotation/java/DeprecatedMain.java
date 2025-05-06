500e4dec5ac4a08f25e353a9aa5779f563473cd4
Kyung-ji-Kang <kangkj001@gmail.com>
2025-05-06 16:53:18 +0900

    20250506

--- src/annotation/java/DeprecatedMain.java
+++ src/annotation/java/DeprecatedMain.java
@@ -6,8 +6,8 @@ public class DeprecatedMain {
         System.out.println("DeprecatedMain.main");
         DeprecatedClass dc = new DeprecatedClass();
         dc.call1();
-        dc.call2(); // IDE 경고
-        dc.call3(); // IDE 경고(*심각) 컴파일 오류같아 보이지만 실행가능함 ㅋㅋ
+        //dc.call2(); // IDE 경고
+        //dc.call3(); // IDE 경고(*심각) 컴파일 오류같아 보이지만 실행가능함 ㅋㅋ
 
     }
 
